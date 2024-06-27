494160f7098b8d31c1c30af183c116f8d20748e4
Marcel <marcel@marcel_laptop.com>
2024-06-27 12:40:47 +0200

    Temperatur Umwandler

---
+++ temperatur_umwandler/temperatur_umwandler.py
@@ -0,0 +1,15 @@
+
+print("(1) Umrechnung von Celsius nach Kelvin")
+print("(2) Umrechnnung von Celsius nach Fahrenheit")
+print("(3) Umrechnung von Kelvin nach Celsius")
+print("(4) Umrechnung von Kelvin nach Fahrenheit")
+print("(5) Umrechnung von Fahrenheit nach Celsius")
+print("(6) Umrechnung von Fahrenheit nach Kelvin")
+
+choice = input("Bitte wählen: ")
+
+if choice == "1":
+    print("Umrechnung von Celsius nach Kelvin")
+    celsius = float(input("Bitte Temperatur in Celsius eingeben: "))
+    
+    
